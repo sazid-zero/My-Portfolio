9303d4ffa40b58ccc3bbc9fb0e6516ef136903fc
sazid-zero <sharif.sazid.3@gmail.com>
2026-02-04 09:25:04 +0600

    about update

--- client/src/pages/portfolio.tsx
+++ client/src/pages/portfolio.tsx
@@ -87,7 +87,6 @@ export default function Portfolio() {
                 <ProfileSection />
                 <ProjectsSection />
                 <SkillsSection />
-                <TestimonialsSection />
                 <ContactSection />
 
                 {/* Footer */}
